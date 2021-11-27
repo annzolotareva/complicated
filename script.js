8379496e23713349c9a4d9390c8b365142fd6733
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-11-27 13:53:51 +0400

    добавила второй способ решения (с использованием reduce)

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let num = String(266219);
-let multiplication = 1;
-for (let i = 0; i < num.length; i++) {
-  multiplication *= num[i];
+//first method
+let num1 = String(266219);
+let multiplication1 = 1;
+for (let i = 0; i < num1.length; i++) {
+  multiplication1 *= num1[i];
 }
-multiplication **= 3; // jshint ignore:line
-multiplication = String(multiplication).substr(0, 2);
-console.log(multiplication);
+multiplication1 **= 3; // jshint ignore:line
+multiplication1 = String(multiplication1).substr(0, 2);
+console.log(multiplication1);
+//second method
+let num2 = Array.from(String(266219), Number);
+let multiplication2 = num2.reduce(function (elem1, elem2) {
+  return elem1 * elem2;
+});
+multiplication2 **= 3; // jshint ignore:line
+multiplication2 = String(multiplication2).substr(0, 2);
+console.log(multiplication2);
