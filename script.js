14a3d36d37b16d61c28935117d39534a7dbe1a5a
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-11-27 14:14:53 +0400

    добавила третий способ решения (с использованием forEach)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//first method
+// first method
 let num1 = String(266219);
 let multiplication1 = 1;
 for (let i = 0; i < num1.length; i++) {
@@ -7,7 +7,7 @@ for (let i = 0; i < num1.length; i++) {
 multiplication1 **= 3; // jshint ignore:line
 multiplication1 = String(multiplication1).substr(0, 2);
 console.log(multiplication1);
-//second method
+// second method
 let num2 = Array.from(String(266219), Number);
 let multiplication2 = num2.reduce(function (elem1, elem2) {
   return elem1 * elem2;
@@ -15,3 +15,12 @@ let multiplication2 = num2.reduce(function (elem1, elem2) {
 multiplication2 **= 3; // jshint ignore:line
 multiplication2 = String(multiplication2).substr(0, 2);
 console.log(multiplication2);
+// third method
+let num3 = Array.from(String(266219), Number);
+let multiplication3 = Array.from(String(1), Number);
+num3.forEach(function (num3) {
+  multiplication3 *= num3;
+});
+multiplication3 **= 3; // jshint ignore:line
+multiplication3 = String(multiplication3).substr(0, 2);
+console.log(multiplication3);
